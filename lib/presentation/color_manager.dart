def95a350edbbfdd3d8d7c5fec7badbcb299fe7e
ShehabAldeen <93327121+ShehabAldeen@users.noreply.github.com>
2023-12-17 12:30:37 +0200

    Creating colorManager class

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ColorManager {
+  static Color primary = const Color(0xffED9728);
+  static Color f525252 = const Color(0xff525252);
+  static Color f737477 = const Color(0xff737477);
+  static Color f9E9E9E = const Color(0xff9E9E9E);
+  static Color f707070 = const Color(0xff707070);
+}
