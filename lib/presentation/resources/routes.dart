31201f558aa30c88f799c6c189f55185bbe434f8
ShehabAldeen <93327121+ShehabAldeen@users.noreply.github.com>
2023-12-19 10:39:17 +0200

    creating app_string file

--- lib/presentation/resources/routes.dart
+++ lib/presentation/resources/routes.dart
@@ -1,3 +1,46 @@
+import 'package:buy/presentation/forget_password/forgetPassword_view.dart';
+import 'package:buy/presentation/login/login_view.dart';
+import 'package:buy/presentation/main/main_view.dart';
+import 'package:buy/presentation/register/register_view.dart';
+import 'package:buy/presentation/resources/app_string.dart';
+import 'package:buy/presentation/splash/splash_view.dart';
+import 'package:buy/presentation/storeDetails/storeDetails_view.dart';
+import 'package:flutter/material.dart';
+
 class Routes {
   static const String splashRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String forgetPasswordRoute = '/forgetPassword';
+  static const String mainRoute = '/main';
+  static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(title: const Text(AppString.noRouteFound)),
+              body: const Center(child: Text(AppString.noRouteFound)),
+            ));
+  }
 }
